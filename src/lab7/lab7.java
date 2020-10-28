a6acb0245e03813fb19efd5621721c6deda1e29d
Nick <70338556+Myroslav-NS@users.noreply.github.com>
2020-10-28 18:30:26 -0400

    Initial commit

---
+++ src/lab7/lab7.java
@@ -0,0 +1,11 @@
+package lab7;
+
+public class lab7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World, this is Nicholas");
+
+	}
+
+}
